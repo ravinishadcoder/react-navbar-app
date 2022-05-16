0a23ef5999e8a20e32289bc5956b79396251b4db
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-05-16 23:57:38 +0530

    navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Logo = ()=>{
+    return <div>
+        <h3>LOGOBAKERY</h3>
+    </div>
+}
+
+const Links = ()=>{
+    return <div className="Links">
+        <h4>Services</h4>
+        <h4>Projects</h4>
+        <h4>About</h4>
+    </div>
+}
+
+const Button = ()=>{
+    return <button>Contact</button>
+}
+
+export {Logo,Links,Button}
